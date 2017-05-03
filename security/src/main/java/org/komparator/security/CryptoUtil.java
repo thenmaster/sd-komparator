eac4d301cca4f19a40f8af55f18bbcee5022c09a
Nuno Gonçalves <nunombg@gmail.com>
2017-05-03 18:53:48 +0100

    Added an intial implementation for signatures. Currently non functional

--- security/src/main/java/org/komparator/security/CryptoUtil.java
+++ security/src/main/java/org/komparator/security/CryptoUtil.java
@@ -3,11 +3,16 @@ package org.komparator.security;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.xml.bind.DatatypeConverter;
 
 public class CryptoUtil {
 
@@ -60,9 +65,45 @@ public class CryptoUtil {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
     	return null;
+    }
 
+    public static String createSignature(String msg, PrivateKey key){
+    	try {
+			Signature s = Signature.getInstance("SHA256withRSA");
+			s.initSign(key);
+			s.update(DatatypeConverter.parseBase64Binary(msg));
+			return DatatypeConverter.printBase64Binary(s.sign());
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return null;
+    }
+
+    public static boolean verifySignature(String sig, String msg, PublicKey key){
+    	try {
+			Signature s = Signature.getInstance("SHA256withRSA");
+			s.initVerify(key);
+			s.update(DatatypeConverter.parseBase64Binary(msg));
+			return s.verify(DatatypeConverter.parseBase64Binary(sig));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return false;
     }
 
 }
--- security/src/main/java/org/komparator/security/handler/DigitalSignatureHandler.java
+++ security/src/main/java/org/komparator/security/handler/DigitalSignatureHandler.java
@@ -1,16 +1,33 @@
 package org.komparator.security.handler;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.Name;
+import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
 import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+import javax.xml.ws.BindingProvider;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+import org.komparator.security.CryptoUtil;
+
+import com.sun.xml.ws.developer.JAXWSProperties;
 
 /**
  * This SOAPHandler outputs the contents of inbound and outbound messages.
@@ -35,8 +52,115 @@ public class DigitalSignatureHandler implements SOAPHandler<SOAPMessageContext> 
 	 * outbound messages.
 	 */
 	@Override
-	public boolean handleMessage(SOAPMessageContext smc) {
-		logToSystemOut(smc);
+	public boolean handleMessage(SOAPMessageContext context) {
+		Boolean outbound = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+
+		if (outbound){
+			SOAPMessage msg = context.getMessage();
+			SOAPPart sp = msg.getSOAPPart();
+			try {
+				SOAPEnvelope se = sp.getEnvelope();
+
+				SOAPHeader sh = se.getHeader();
+				if (sh == null)
+					sh = se.addHeader();
+
+				Name name = se.createName("Signature", "s", "http://sig");
+				SOAPHeaderElement element = sh.addHeaderElement(name);
+
+				String svcn = (String) context.get(BindingProvider.ENDPOINT_ADDRESS_PROPERTY);
+
+				String filename = "/A24_Supplier";
+				String alias = "a24_supplier";
+
+				System.out.println(msg.getSOAPBody().getTextContent());
+				System.out.println(context.get(JAXWSProperties.HTTP_REQUEST_URL));
+				System.out.println(context.get(BindingProvider.ENDPOINT_ADDRESS_PROPERTY));
+
+				if(svcn.contains("8081")){
+					filename += "1";
+					alias += "1";
+				}
+				else if(svcn.contains("8082")){
+					filename += "2";
+					alias += "2";
+				}
+				else if(svcn.contains("8083")){
+					filename += "3";
+					alias += "3";
+				}
+				else // supplier without any keys
+					return false;
+
+				filename += ".jks";
+
+				System.out.println(filename + "   " + alias);
+
+				InputStream is = this.getClass().getResourceAsStream(filename);
+
+				PrivateKey key = null;
+
+				try {
+					KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+					keystore.load(is, "f19Ho2MJ".toCharArray());
+					key = (PrivateKey) keystore.getKey(alias, "f19Ho2MJ".toCharArray());
+				} catch (KeyStoreException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (CertificateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnrecoverableKeyException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				String sigString = CryptoUtil.createSignature(msg.getSOAPBody().getTextContent(),key);
+				element.addTextNode(sigString);
+			} catch (SOAPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else{
+			SOAPMessage msg = context.getMessage();
+			SOAPPart sp = msg.getSOAPPart();
+			try {
+				SOAPEnvelope se = sp.getEnvelope();
+				SOAPHeader sh = se.getHeader();
+
+				// check header
+				if (sh == null) {
+					System.out.println("Header not found.");
+					return true;
+				}
+
+				/*
+			Name name = se.createName("Signature", "s", "http://sig");
+
+			Iterator it =  sh.getChildElements(name);
+
+			if (!it.hasNext()) {
+				System.out.println("Header element not found.");
+				return true;
+			}
+
+			SOAPElement element = (SOAPElement) it.next();
+
+			String sigString = element.getValue();
+
+			//TODO Verificar sig string
+			*/
+			} catch (SOAPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 		return true;
 	}
 
@@ -72,7 +196,7 @@ public class DigitalSignatureHandler implements SOAPHandler<SOAPMessageContext> 
 		System.out.print("[");
 		System.out.print(dateFormatter.format(new Date()));
 		System.out.print("] ");
-		
+
 		System.out.print("intercepted ");
 		if (outbound)
 			System.out.print("OUTbound");
