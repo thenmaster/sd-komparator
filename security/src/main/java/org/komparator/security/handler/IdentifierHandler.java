40418264409153785729aec60b4abc16f0f5deba
Nuno Gonçalves <nunombg@gmail.com>
2017-05-15 13:58:41 +0100

    Added a new handler that will analyse unique ids.

---
+++ security/src/main/java/org/komparator/security/handler/IdentifierHandler.java
@@ -0,0 +1,54 @@
+package org.komparator.security.handler;
+
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+/**
+ * This SOAPHandler adds a unique id to messages from the mediator client to the current primary mediator.
+ */
+public class IdentifierHandler implements SOAPHandler<SOAPMessageContext> {
+
+	/**
+	 * Gets the header blocks that can be processed by this Handler instance. If
+	 * null, processes all.
+	 */
+	@Override
+	public Set<QName> getHeaders() {
+		return null;
+	}
+
+	/**
+	 * The handleMessage method is invoked for normal processing of inbound and
+	 * outbound messages.
+	 */
+	@Override
+	public boolean handleMessage(SOAPMessageContext context) {
+		Boolean outbound = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+
+		if (outbound){
+			//TODO add code to add the id
+		}else{
+			//TODO add code to verify the id is new
+		}
+		return true;
+	}
+
+	/** The handleFault method is invoked for fault message processing. */
+	@Override
+	public boolean handleFault(SOAPMessageContext smc) {
+		return true;
+	}
+
+	/**
+	 * Called at the conclusion of a message exchange pattern just prior to the
+	 * JAX-WS runtime dispatching a message, fault or exception.
+	 */
+	@Override
+	public void close(MessageContext messageContext) {
+		// nothing to clean up
+	}
+}
